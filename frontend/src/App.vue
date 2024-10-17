6268dced4b6eba85991ce6c9c275b23364078675
christoph3r3w <144007933+christoph3r3w@users.noreply.github.com>
2024-10-17 23:18:31 +0200

    app.vue
    added
    - general styling
    - imported and declared header ad footer
    - router view implementation for SPA

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,61 @@
-<script setup>
-</script>
 
 <template>
 
-<h1>
-  hello world
-</h1>
+  <div class="body">
+    <Header class="header" />
   
+    <router-view />
+  
+    
+    <Footer class="footer" />
+
+  </div>
+    
 </template>
 
-<style scoped>
+<script>
+    import Header from './components/molecule component/head-nav.vue'
+    import Footer from './components/molecule component/footer.vue'
+    
+      export default {
+        name: 'app',
+    
+        components: {
+          Header,
+          Footer,
+        }
+      }
+    
+</script>
+
+
+
+<style>
+
+.body{
+  min-height: 100vh;
+  position: relative;
+  /* color: white ; */
+
+}
+
+.header,.footer{
+  background-color: black;
+  color: var(--LD-text,white);
+  color: white;
+  padding: 3%;
+}
+
+.footer{
+  position: absolute;
+  bottom: 0;
+  left:0;
+  right:0;
+}
+
+
+
+
 
 
 </style>
