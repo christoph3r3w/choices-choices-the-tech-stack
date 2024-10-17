a0803da2abbdbc3ae18d2005e78d3f9ac6c01018
christoph3r3w <144007933+christoph3r3w@users.noreply.github.com>
2024-10-18 00:24:41 +0200

    Routing and Axios
    - implemented router and component routing
    - imported axios

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,5 +2,19 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import "./axios.js";
 
-createApp(App).mount('#app')
+const router = createRouter({
+	history: createWebHistory(),
+
+	routes: [
+		{ path: "/", name: Home, component: Home },
+		{ path: "/login", name: Login, component: Login },
+		{ path: "/profile", name: Profile, component: Profile },
+	],
+});
+
+createApp(App)
+	.use(router)
+
+	.mount("#app");
---
+++ frontend/src/router.js
@@ -0,0 +1,16 @@
+// import Vue from 'vue';
+// import {createRouter,createWebHashHistory} from 'vue-router'
+// import Home from './components/home.vue'
+// import Login from './components/login.vue'
+// import Profile from './components/profile.vue'
+
+
+
+
+// export default new router({
+// 	routes: [
+// 		{path:'/', component: Home},
+// 		{path:'/login', component: Login},
+// 		{path:'/profile', component: Profile},
+// 	]
+// })
