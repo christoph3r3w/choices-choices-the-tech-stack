847ad7940a45c008df7b201f73d9f39c2c1f51b0
christoph3r3w <144007933+christoph3r3w@users.noreply.github.com>
2024-10-17 23:15:56 +0200

    main.js
    - imported components
    - established router
    - imported axios

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,24 @@
-import './assets/main.css'
+import "./assets/main.css";
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "./components/home.vue";
+import Login from "./components/login.vue";
+import Profile from "./components/profile.vue";
+import "./axios.js";
 
-createApp(App).mount('#app')
+const router = createRouter({
+	history: createWebHistory(),
+
+	routes: [
+		{ path: "/", name: Home, component: Home },
+		{ path: "/login", name: Login, component: Login },
+		{ path: "/profile", name: Profile, component: Profile },
+	],
+});
+
+createApp(App)
+	.use(router)
+
+	.mount("#app");
