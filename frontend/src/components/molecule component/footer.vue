8956f4b789e7e4b7d1ca9ea8dd6e3eac09c094eb
christoph3r3w <144007933+christoph3r3w@users.noreply.github.com>
2024-10-18 12:30:37 +0200

    component adjustments
    - changed window component to molecule folder
    - adjusted button component
    - added footer content

--- frontend/src/components/molecule component/footer.vue
+++ frontend/src/components/molecule component/footer.vue
@@ -1,10 +1,28 @@
 <template>
-	<div>
-		footer
-	</div>
+	<footer>
+    <p style="z-index: 3;">
+        &#169; 2024 Deloitte <a href="/">See Terms of Use </a>for more information.
+    </p>
+</footer>
 
 </template>
 
 <script>
 
 </script>
+
+
+
+<style scoped>
+
+	footer{
+		position: relative;
+		display: flex;
+		align-items: center;
+	}
+
+	a{
+		color: var(--D-t-support);
+	}
+
+</style>
---
+++ frontend/src/components/molecule component/window.vue
@@ -0,0 +1,27 @@
+<template>
+	<div>
+		<h1></h1>
+		<p></p>
+		<slot></slot>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'Window',
+	};
+</script>
+
+
+<style scoped>
+	div{
+		background-color:  rgba(255, 0, 0, 0.21);
+		/* background-color: oklch(94.05% 0.0161 58.63); */
+		height: 100%;
+		width: 100%;
+		padding: 5%;
+
+	}
+
+
+</style>
