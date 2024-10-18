373d0ceac33afa8d9feb251a18266c99c622362e
christoph3r3w <144007933+christoph3r3w@users.noreply.github.com>
2024-10-18 12:33:24 +0200

    Update App.vue
    - adjusted component path
    - styling adjustments for home

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,7 +3,7 @@
   <div class="body">
     <Header class="header" />
   
-    <router-view />
+    <router-view class="content" />
   
     
     <Footer class="footer" />
@@ -13,8 +13,8 @@
 </template>
 
 <script>
-    import Header from './components/molecule component/head-nav.vue'
-    import Footer from './components/molecule component/footer.vue'
+    import Header from '@/components/molecule component/head-nav.vue'
+    import Footer from '@/components/molecule component/footer.vue'
     
       export default {
         name: 'app',
@@ -35,21 +35,31 @@
   min-height: 100vh;
   position: relative;
   /* color: white ; */
+  display: flex;
+  flex-direction: column;
+  background-color: color(display-p3 0.9494 0.9152 0.886) ;
 
 }
 
 .header,.footer{
-  background-color: black;
   color: var(--LD-text,white);
-  color: white;
-  padding: 3%;
+  padding-inline: 2.5%;
+  flex:0 1 5vh;
 }
 
 .footer{
-  position: absolute;
+  background-color: black;
+  color: white;
+  
+  /* position: absolute; */
   bottom: 0;
   left:0;
   right:0;
+}
+
+.content{
+  flex: 1;
+
 }
 
 
