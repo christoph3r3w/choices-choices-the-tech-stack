2099db863a3b3a6eade39c24d7b896668be92d9e
christoph3r3w <144007933+christoph3r3w@users.noreply.github.com>
2024-10-18 12:49:18 +0200

    profile
    - added window component
    - content template
    - styling

--- frontend/src/components/profile.vue
+++ frontend/src/components/profile.vue
@@ -1,12 +1,33 @@
 <template>
-	<div>
-		profile page
-		<router-link to="/login">log-out</router-link>
-	</div>
+		<Window >
+			<div class="window-container">
+				profile page
+				<router-link to="/login">log-out</router-link>
+				<router-link to="/wizard">wizardt</router-link>
+			</div>
+
+		</Window>
 </template>
 
 <script>
-	export default{
-		name:'Profile'
+	import Window from '@/components/molecule component/window.vue'
+
+	export default {
+		name:'Profile',
+		components:{
+			Window
+		}
 	}
 </script>
+
+
+<style>
+
+	.window-container{
+		display: flex;
+		flex-direction: column;
+		gap:3rem;
+	}
+
+
+</style>
