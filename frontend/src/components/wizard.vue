3db464badaafe927b29742c9cae380020fbef219
christoph3r3w <144007933+christoph3r3w@users.noreply.github.com>
2024-10-21 17:22:41 +0200

    wizard component
    added
    - styling
    - buttons logic
    - wizard question loop function

--- frontend/src/components/wizard.vue
+++ frontend/src/components/wizard.vue
@@ -1,17 +1,37 @@
 <template>
-	<Window>
-		<div class="window-list" v-for="(wini,winiIndex) in 9" :key="winiIndex">
-			<label v-for=" n in 6" :key="n" :for="`wini${winiIndex + 1}-check${n}`">
-				<input type="radio" :name="`wini${winiIndex}`" :id="`wini${winiIndex + 1}-check${n}`" required />
+	<Window
+		class="window-box"
+		:title="'wizard'"
+		id="wizard"
+		:context1="'read and choose carefully'"
+	>
+		<div class="window-list" v-for="(wini,winiIndex) in 11" :key="winiIndex">
+			<label v-for=" n in 5" 
+			:key="n" 
+			:for="`wini${winiIndex + 1}-check${n}`" 
+			>
+				<input 
+				type="radio" 
+				:name="`wini${winiIndex}`" 
+				:id="`wini${winiIndex + 1}-check${n}`" 
+				:value="n"
+				v-model="winiSelect[winiIndex]"
+				required />
 			</label>
 		</div>
 
 		<nav class="button-box">
-			<Button>submit</Button>
-			<Button>restard</Button>
+			<Button 
+			:text="'Submit'"
+			:task="''"
+			 @click.prevent="handleSubmit" 
+			 />
 
-			<Checkbox/>
-
+			<Button 
+			:text="'Restart agian'" 
+			:task="''"
+			@click="handleRestart" />
+			<!-- <Checkbox/> -->
 		</nav>
 	</Window>
 </template>
@@ -19,6 +39,7 @@
 <script>
 import Window from "@/components/molecule component/window.vue";
 import Button from "@/components/atom components/button.vue";
+import axios from "axios";
 // import Checkbox from "@/components/atom components/checkbox.vue";
 
 export default {
@@ -29,10 +50,59 @@ export default {
 		// Checkbox,
 		Button,
 	},
+	data(){
+		return{
+			winiSelect: Array(11).fill(null),
+		}
+	},
+
+	methods: {
+		async handleSubmit(e) {
+			e.preventDefault();
+			
+		
+			const selections = this.winiSelect.map((value, index) => ({
+				windowId: index + 1,
+				selection: value
+			})).filter(item => item.selection !== null);
+			console.log('data', selections);
+			
+			try {
+				const response = await axios.post('/users', {
+					wini: selections,
+				});
+				
+				if (response.status === 201) {
+					this.$router.push('/profile');
+					this.$emit('click');
+					console.log(response);
+				} else {
+					console.warn('not good');
+				}
+			} catch (error) {
+				console.error('Error submitting form:', error);
+			}
+			// go to #top
+			window.location.hash = 'top';
+		},
+
+		handleRestart() {
+			window.location.reload();
+			
+			window.location.hash = '';
+
+
+		},
+	},
 };
 </script>
 
 <style scoped>
+
+	.window-box{
+		border-radius: 50px 50px 0 0;
+		background-color: var(--D-light-bk);
+	}
 
 	.window-list{
 		display: flex;
@@ -41,7 +111,11 @@ export default {
 		height: auto;
 		/* justify-content: space-between; */
 		/* outline: solid green; */
-		outline: solid red;
+		/* outline: solid red; */
+		filter: grayscale(1);
+		opacity: .4;
+		/* background-color: rgba(0, 0, 0, 0.514); */
+		pointer-events: none;
 	}
 	
 	label{
@@ -51,7 +125,7 @@ export default {
 		height: 5rem;
 		place-content: center;
 		outline: solid grey;
-		opacity: .4;
+		/* opacity: .4; */
 
 		border-radius: 10px;
 	}
@@ -61,11 +135,43 @@ export default {
 		opacity: 1;
 	}
 
+	/* label:has(input:checked) + label{
+		background-color: var(--D-mid-bk);
+		opacity: 1;
+	} */
+
+	.window-list:nth-of-type(1){
+		outline:none ;
+		filter: grayscale(0);
+		opacity: 1;
+		pointer-events: initial;
+
+		& label:hover{
+			background-color: var(--D-dark-support);
+		}
+
+
+	}
 
 
 	.window-list:has(input:checked) {
 		outline: none;
+		filter: grayscale(0);
+		opacity: 1;
 
+
+
+	}
+
+	.window-list:has(input:checked) +.window-list {
+		outline: none;
+		filter: grayscale(0);
+		opacity: 1;
+		pointer-events: initial;
+
+		& label:hover{
+			background-color: var(--D-t-support);
+		}
 	}
 
 </style>
