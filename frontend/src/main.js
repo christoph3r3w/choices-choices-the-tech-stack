ab14ca3ca2c86819999127744c9e8fa5964a8c71
christoph3r3w <144007933+christoph3r3w@users.noreply.github.com>
2024-10-21 17:12:35 +0200

    redirect from homepage to login

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,17 +6,15 @@ import App from "./App.vue";
 import Home from "./components/home.vue";
 import Login from "./components/login.vue";
 import Profile from "./components/profile.vue";
-import Wizard from "./components/wizard.vue";
 import "./axios.js";
 
 const router = createRouter({
 	history: createWebHistory(),
 
 	routes: [
-		{ path: "/", name: Home, component: Home },
+		{ path: "/",redirect:'/login', name: Home, component: Home },
 		{ path: "/login", name: Login, component: Login },
 		{ path: "/profile", name: Profile, component: Profile },
-		{ path: "/wizard", name:Wizard,component:Wizard},
 	],
 });
 
